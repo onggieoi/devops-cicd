36c9bf2a6b78d54147f0b54d78d15fea77c1b37b
Duc Cao <duc.caotranquang@nashtechglobal.com>
2023-08-15 17:55:09 +0700

    add root pipe line

---
+++ vars/buildBackEnd.groovy
@@ -0,0 +1,82 @@
+#!/usr/bin/env groovy
+void call() {
+    String name = "backend"
+    String buildFolder = "backend"
+    String baseImage     = "node"
+    String baseTag       = "lts-buster"
+    String demoRegistry = "361555779387.dkr.ecr.ap-southeast-1.amazonaws.com"
+    String awsRegion = "ap-south-1"
+    String ecrRegistryUrl = "https://361555779387.dkr.ecr.ap-southeast-1.amazonaws.com"
+    String awsCredential = 'aws-credentials'
+
+//========================================================================
+//========================================================================
+
+//========================================================================
+//========================================================================
+
+    stage ('Prepare Package') {
+        script {
+            writeFile file: '.ci/Dockerfile', text: libraryResource('node/Dockerfile')
+        }
+    }
+
+    stage('SonarQube analysis') {
+        echo "Run SonarQube Analysis"
+    }
+
+    stage ("Build Solution") {
+        docker.build("ecr-duccao-${name}:${BUILD_NUMBER}", " -f ./.ci/Dockerfile \
+        --build-arg BASEIMG=${baseImage} --build-arg IMG_VERSION=${baseTag} ${WORKSPACE}/src/${buildFolder}") 
+    }
+
+    stage ('Run Unit Tests') {
+        echo "Run Unit Tests"
+    }
+
+    stage ('Run Integration Tests') {
+        echo "Run Integration Tests"
+    }
+
+    stage ('Process Test Results') {
+        echo "Export Test Results"
+    }
+
+    stage ("Push Docker Images") {
+        echo "Push Docker Images"
+        docker.withRegistry(ecrRegistryUrl, "ecr:${awsRegion}:${awsCredential}") {
+            sh "docker tag ecr-duccao-devops-${name}:${BUILD_NUMBER} ${demoRegistry}/ecr-duccao-devops-${name}:${BUILD_NUMBER}"
+            sh "docker push ${demoRegistry}/ecr-duccao-devops-${name}:${BUILD_NUMBER}"
+            sh "docker tag ${demoRegistry}/ecr-duccao-devops-${name}:${BUILD_NUMBER} ${demoRegistry}/ecr-duccao-devops-${name}:latest"
+            sh "docker push ${demoRegistry}/ecr-duccao-devops-${name}:latest"
+        }
+    }
+
+    stage ('Prepare Package') {
+        script {
+            writeFile file: '.ci/service/deployment.yml', text: libraryResource('deploy/eks/service/deployment.yml')
+            writeFile file: '.ci/service/service.yml', text: libraryResource('deploy/eks/service/service.yml')
+        }
+    }
+
+    stage ("Deploy BackEnd To K8S") {
+        docker.withRegistry(ecrRegistryUrl, "ecr:${awsRegion}:${awsCredential}") {
+            sh "export registry=${demoRegistry}; export appname=${name}; export tag=latest; \
+            envsubst < .ci/service/deployment.yml > deployment.yml; envsubst < .ci/service/service.yml > service.yml"
+            sh "aws eks --region ${awsRegion} update-kubeconfig --name ${eksName}"
+            sh "kubectl apply -f deployment.yml"
+            sh "kubectl apply -f service.yml"
+        }
+    }
+}
+
+//========================================================================
+// node CI
+// Version: v1.0
+// Updated:
+//========================================================================
+//========================================================================
+// Notes:
+//
+//
+//========================================================================
---
+++ vars/rootPipeline.groovy
@@ -0,0 +1,74 @@
+#!/usr/bin/env groovy
+void call(Map pipelineParams) {
+
+    pipeline {
+
+        agent any
+
+        options {
+            disableConcurrentBuilds()
+            disableResume()
+            timeout(time: 1, unit: 'HOURS')
+        }
+        
+        stages {
+            stage ('Build Backend') {
+                when {
+                    allOf {
+                        // Condition Check
+                        anyOf{
+                            // Branch Event: Nornal Flow
+                            anyOf {
+                                branch 'master'
+                            }
+                            // Manual Run: Only if checked.
+                            allOf{
+                                changeset "src/backend/**"
+                            }
+                        }
+                    }
+                }
+                steps {
+                    script {
+                        buildBackEnd()
+                    }
+                }
+            }
+            // stage ('Build Frontend') {
+            //     when {
+            //         allOf {
+            //             anyOf{
+            //                 anyOf {
+            //                     branch 'master'
+            //                 }
+            //                 allOf{
+            //                     changeset "src/frontend/**"
+            //                 }
+            //             }
+            //         }
+            //     }
+            //     steps {
+            //         script {
+            //             buildFrontend()
+            //         }
+            //     }
+            // }
+        }
+
+        post {
+            cleanup {
+                cleanWs()
+            }
+        }
+    }
+}
+//========================================================================
+// Demo CI
+// Version: v1.0
+// Updated:
+//========================================================================
+//========================================================================
+// Notes:
+//
+//
+//========================================================================
