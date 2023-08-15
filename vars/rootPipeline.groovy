d1f8e1bab269a6ee8c8c78ea7b979ba0f6277f8a
Duc Cao <duc.caotranquang@nashtechglobal.com>
2023-08-15 22:10:11 +0700

    fixed naming

--- vars/rootPipeline.groovy
+++ vars/rootPipeline.groovy
@@ -50,7 +50,7 @@ void call(Map pipelineParams) {
                 }
                 steps {
                     script {
-                        buildFrontend()
+                        buildFrontEnd()
                     }
                 }
             }
